19e6de65f84b49f6b92a13cea86d5bb6b65773d9
mariyasaji-2023 <mariyasajichiramel.com>
2026-03-09 18:28:24 +0530

    CORS

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const connectDB = require("./src/config/db");
 
 dotenv.config();
 connectDB();
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
