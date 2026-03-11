c7820f577e36fe4f1cdc72548e1f0c67b2174f87
mariyasaji-2023 <mariyasajichiramel.com>
2026-03-11 15:41:16 +0530

    Edit getAdminDetails

--- src/routes/superAdminRoutes.js
+++ src/routes/superAdminRoutes.js
@@ -6,6 +6,6 @@ const protect = require("../middleware/authMiddleware");
 router.post("/create-admin", adminController.createAdmin);
 router.post("/login", adminController.loginAdmin);
 router.put("/update-admin", protect, adminController.updateAdmin);
-router.get("/adminDetails",protect,adminController.getAdminProfile)
+router.get("/getAdminDetails",protect,adminController.getAdminProfile)
 
 module.exports = router;
